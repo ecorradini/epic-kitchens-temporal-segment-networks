ac584c85a22d1ade3c4c33b626fe6055431f5aec
Enrico Corradini <ecorradini@PC192.168.0.112.homenet.telecomitalia.it>
2018-07-03 21:01:07 +0200

    bugfix

--- build_all_f.sh
+++ build_all_f.sh
@@ -39,11 +39,50 @@ sed -i -e '85i                        OUTPUT_STRIP_TRAILING_WHITESPACE)' ../cmak
 sed -i -e '86i        string(REGEX MATCH \"[0-9]+\\\\.[0-9]+\\\\.[0-9]+" CMAKE_GCC_REGEX_VERSION "${CMAKE_OPENCV_GCC_VERSION_FULL}")' ../cmake/OpenCVDetectCXXCompiler.cmake
 sed -i -e '87i    endif()\' ../cmake/OpenCVDetectCXXCompiler.cmake
 
+sed -i -e '47i#define AV_CODEC_FLAG_GLOBAL_HEADER (1 << 22)\' ../modules/highgui/src/cap_ffmpeg_impl.hpp
+sed -i -e '48i#define CODEC_FLAG_GLOBAL_HEADER AV_CODEC_FLAG_GLOBAL_HEADER\' ../modules/highgui/src/cap_ffmpeg_impl.hpp
+sed -i -e '49i#define AVFMT_RAWPICTURE 0x0020\' ../modules/highgui/src/cap_ffmpeg_impl.hpp
+
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D WITH_TBB=ON -D WITH_V4L=ON -DENABLE_PRECOMPILED_HEADERS=OFF ..
 if make -j32 ; then
     cp lib/cv2.so ../../../
     echo "OpenCV" $version "built."
 else
     echo "Failed to build OpenCV. Please check the logs above."
+    exit 1
+fi
+
+# build dense_flow
+cd ../../../
+
+echo "Building Dense Flow"
+cd lib/dense_flow
+[[ -d build ]] || mkdir build
+cd build
+OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF
+if make -j ; then
+    echo "Dense Flow built."
+else
+    echo "Failed to build Dense Flow. Please check the logs above."
+    exit 1
+fi
+
+# build caffe
+echo "Building Caffe, MPI status: ${CAFFE_USE_MPI}"
+cd ../../caffe-action
+[[ -d build ]] || mkdir build
+cd build
+sudo dnf -y install openblas-devel openmpi-devel
+if [ "$CAFFE_USE_MPI" == "MPI_ON" ]; then
+OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DUSE_MPI=ON -DMPI_CXX_COMPILER="${CAFFE_MPI_PREFIX}/bin/mpicxx" -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF -DBLAS=open
+else
+OpenCV_DIR=../../../3rd-party/opencv-$version/build/ cmake .. -DCUDA_USE_STATIC_CUDA_RUNTIME=OFF -DBLAS=open
+fi
+if make -j32 install ; then
+    echo "Caffe Built."
+    echo "All tools built. Happy experimenting!"
+    cd ../../../
+else
+    echo "Failed to build Caffe. Please check the logs above."
     exit 1
 fi
