72b25a4db15e6cd5227d7668fbfcee18aee32131
ecorradini <enrico.corradini94@gmail.com>
2018-07-04 16:50:00 +0200

    added solvers

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-lib/caffe-action/build/tools/caffe train --solver=models/tsn_bn_inception_rgb_solver.prototxt >> output.txt 2>&1
+lib/caffe-action/build/tools/caffe train --solver=models/epic_kitchens_solver_1.prototxt >> output.txt 2>&1
 
