effbc67bc00cdb6903952b84a2fd4307359df6ac
ecorradini <enrico.corradini94@gmail.com>
2018-07-06 16:32:28 +0200

    Istruzioni

--- train.sh
+++ train.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 lib/caffe-action/build/tools/caffe train --solver=models/epic_kitchens_solver_1.prototxt >> output.txt 2>&1
+lib/caffe-action/build/tools/caffe train --solver=models/epic_kitchens_solver_2.prototxt --snapshot=models/epic_kitchens_model_iter_61.solverstate >> output.txt 2>&1
+lib/caffe-action/build/tools/caffe train --solver=models/epic_kitchens_solver_3.prototxt --snapshot=models/epic_kitchens_model_iter_123.solverstate >> output.txt 2>&1
 
